a321802920156237ed1ecdff21d5e58fb7cbff6d
TranThanhTin-Stc4594 <tin.tranthanh@sasi.vn>
2024-07-02 11:16:13 +0700

    machine
    Machine1

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ function checkPassword() {
         alert('Incorrect password!');
     }
 }
+
 function checkEnter(event) {
     if (event.key === 'Enter') {
         checkPassword();
@@ -19,33 +20,15 @@ function hien() {
     var hideElement = document.getElementById('hide');
     hideElement.classList.toggle('hidden');
 }
+
+// Prevent context menu
 document.addEventListener('contextmenu', event => event.preventDefault());
-document.onkeypress = function (event) {
-    event = (event || window.event);
-    if (event.keyCode == 123) {
-    return false;
-    }
-    }
-    document.onmousedown = function (event) {
-    event = (event || window.event);
-    if (event.keyCode == 123) {
-    return false;
-    }
-    }
-    document.onkeydown = function (event) {
-    event = (event || window.event);
-    if (event.keyCode == 123) {
-    return false;
-    }
-    }
-    jQuery(document).ready(function($){
-        $(document).keydown(function(event) {
-        var pressedKey = String.fromCharCode(event.keyCode).toLowerCase();
-        
-        if (event.ctrlKey && (pressedKey == "c" || pressedKey == "u")) {
+
+// Prevent F12, Ctrl+U, Ctrl+C
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'F12' || 
+        (event.ctrlKey && (event.key === 'u' || event.key === 'c'))) {
+        event.preventDefault();
         alert('Sorry, This Functionality Has Been Disabled!');
-        //disable key press porcessing
-        return false;
-        }
-        });
-        });
+    }
+});
